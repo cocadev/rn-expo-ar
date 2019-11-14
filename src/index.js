7fc13936d40a8065f27d25a385888dc2b035e832
John <40970351+funnyjerry@users.noreply.github.com>
2019-11-15 00:08:51 +0800

    add camera

--- src/index.js
+++ src/index.js
@@ -1,41 +1,59 @@
 import React from 'react'
 import { Text, View } from 'react-native'
-// import Permissions related logic from Expo library
+// Use Camera component from Expo library
 import * as Permissions from 'expo-permissions';
+import { Camera } from 'expo-camera';
 
+const cameraStyle = { flex: 1 } // fill the whole screen with camera component
 const flexCenterStyle = { flex: 1, justifyContent: 'center', alignItems: 'center' }
 
 class MainView extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-       // initialize flag for camera permission
       hasCameraPermission: null
     }
-    // bind the camera permission callback
-    // to set the correct context
     this.onCameraPermission = this.onCameraPermission.bind(this)
   }
 
   componentDidMount() {
-    // when component is initialized
-    // user is asked for permission to use the camera
     Permissions
       .askAsync(Permissions.CAMERA)
       .then(this.onCameraPermission)
   }
 
   onCameraPermission({ status }) {
-    // camera permission flag is updated once
-    // when the user has dismissed the dialog
     this.setState({ hasCameraPermission: status === 'granted' })
   }
 
   render() {
+    // Use destructuring to read camera permission value
+    const { hasCameraPermission } = this.state;
+
+    // Flag not initialized, permission dialog is not yet dismissed.
+    // -> Return an empty view.
+    if (hasCameraPermission === null) {
+      return <View />
+    }
+
+    // User did not give permission to use the camera
+    // -> Show error message
+    if (hasCameraPermission === false) {
+      return (
+        <View style={flexCenterStyle}>
+          <Text>No access to camera</Text>
+        </View>
+        )
+    }
+
+    // Camera permission ok, show camera component
     return (
-    <View style={flexCenterStyle}>
-      <Text>Hello World!</Text>
-    </View>
+      <View style={cameraStyle}>
+        <Camera
+          style={cameraStyle}
+          type={Camera.Constants.Type.front}
+        />
+      </View>
     )
   }
 }
