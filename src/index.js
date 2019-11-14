617d62ba8f697b197570ea50fbc2f2f846ca2985
John <40970351+funnyjerry@users.noreply.github.com>
2019-11-15 00:12:45 +0800

    facedetector

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
+
+
 import React from 'react'
 import { Text, View } from 'react-native'
-// Use Camera component from Expo library
 import * as Permissions from 'expo-permissions';
 import { Camera } from 'expo-camera';
+import * as FaceDetector from 'expo-face-detector';
 
-const cameraStyle = { flex: 1 } // fill the whole screen with camera component
+const cameraStyle = { flex: 1 }
 const flexCenterStyle = { flex: 1, justifyContent: 'center', alignItems: 'center' }
 
 class MainView extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      hasCameraPermission: null
+      hasCameraPermission: null,
+      faces: [] // initialize found faces with empty array
     }
     this.onCameraPermission = this.onCameraPermission.bind(this)
+    // bind the callbacks
+    // to set the correct context
+    this.onFacesDetected = this.onFacesDetected.bind(this)
+    this.onFaceDetectionError = this.onFaceDetectionError.bind(this)
   }
 
   componentDidMount() {
@@ -26,18 +33,26 @@ class MainView extends React.Component {
     this.setState({ hasCameraPermission: status === 'granted' })
   }
 
+  // implement face detection callback function
+  onFacesDetected({ faces }) {
+    // print the found face data to console
+    console.log(faces)
+    // store faces to component state
+    this.setState({ faces })
+  }
+
+  // implement face detection error function
+  onFaceDetectionError(error) {
+    console.log(error)
+  }
+
   render() {
-    // Use destructuring to read camera permission value
     const { hasCameraPermission } = this.state;
 
-    // Flag not initialized, permission dialog is not yet dismissed.
-    // -> Return an empty view.
     if (hasCameraPermission === null) {
       return <View />
     }
 
-    // User did not give permission to use the camera
-    // -> Show error message
     if (hasCameraPermission === false) {
       return (
         <View style={flexCenterStyle}>
@@ -46,12 +61,24 @@ class MainView extends React.Component {
         )
     }
 
-    // Camera permission ok, show camera component
     return (
       <View style={cameraStyle}>
+        {/*
+          Enable face detector in camera component
+          mode: fast or accurate,
+          detectLandmarks: whether to get the eyes, nose, mouth position
+          runClassifications: eyes open and smiling probability
+         */}
         <Camera
           style={cameraStyle}
           type={Camera.Constants.Type.front}
+          faceDetectorSettings={{
+            mode: FaceDetector.Constants.Mode.fast,
+            detectLandmarks: FaceDetector.Constants.Landmarks.all,
+            runClassifications: FaceDetector.Constants.Classifications.all
+          }}
+          onFacesDetected={this.onFacesDetected}
+          onFacesDetectionError={this.onFacesDetectionError}
         />
       </View>
     )
