e50747d5f2b39a78e65aae470ce981f031b44ce2
John <40970351+funnyjerry@users.noreply.github.com>
2019-11-15 00:07:15 +0800

    add camera permissions to expo

--- App.js
+++ App.js
@@ -1,19 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Camera from './src/index'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Camera />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- src/index.js
+++ src/index.js
@@ -1,20 +1,40 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+// import Permissions related logic from Expo library
+import * as Permissions from 'expo-permissions';
 
 const flexCenterStyle = { flex: 1, justifyContent: 'center', alignItems: 'center' }
 
 class MainView extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+       // initialize flag for camera permission
+      hasCameraPermission: null
+    }
+    // bind the camera permission callback
+    // to set the correct context
+    this.onCameraPermission = this.onCameraPermission.bind(this)
   }
 
   componentDidMount() {
+    // when component is initialized
+    // user is asked for permission to use the camera
+    Permissions
+      .askAsync(Permissions.CAMERA)
+      .then(this.onCameraPermission)
+  }
+
+  onCameraPermission({ status }) {
+    // camera permission flag is updated once
+    // when the user has dismissed the dialog
+    this.setState({ hasCameraPermission: status === 'granted' })
   }
 
   render() {
     return (
     <View style={flexCenterStyle}>
-      <Text>Panda Sunglassess!</Text>
+      <Text>Hello World!</Text>
     </View>
     )
   }
