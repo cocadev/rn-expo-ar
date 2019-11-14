1b34c91df13f63f756104afef3230b637bef89ed
John <40970351+funnyjerry@users.noreply.github.com>
2019-11-15 00:15:11 +0800

    add eyes to face

--- src/Mask/index.js
+++ src/Mask/index.js
@@ -2,10 +2,52 @@ import React from 'react'
 
 import { View } from 'react-native';
 
-const Mask = ({ face }) => {
-  console.log(face)
+// Destructure face data given as argument
+const Mask = ({
+  face: {
+    bounds: {
+      origin: { x: containerX, y: containerY },
+      size: { width: faceWidth }
+    },
+    leftEyePosition,
+    rightEyePosition
+  }
+}) => {
+  // Define the helpers for calculating the correct positions
+  const eyeWidth = faceWidth / 4
+
+  const translatedEyePositionX = eyePosition => eyePosition.x - eyeWidth / 2 - containerX
+  const translatedEyePositionY = eyePosition => eyePosition.y - eyeWidth / 2 - containerY
+
+  const translatedLeftEyePosition = {
+    x: translatedEyePositionX(leftEyePosition),
+    y: translatedEyePositionY(leftEyePosition)
+  }
+  const translatedRightEyePosition = {
+    x: translatedEyePositionX(rightEyePosition),
+    y: translatedEyePositionY(rightEyePosition)
+  }
+
+  // Define the style for the eye component
+  const eyeStyle = (eyePosition, eyeBorderWidth = eyeWidth / 10) => ({
+    position: 'absolute',
+    left: eyePosition.x,
+    top: eyePosition.y,
+    borderRadius: eyeWidth,
+    width: eyeWidth,
+    height: eyeWidth,
+    borderWidth: eyeBorderWidth,
+    borderColor: 'black',
+    backgroundColor:'yellow'
+  });
+
   return (
-    <View />
+    <View style={{ position: 'absolute', left: containerX, top: containerY }}>
+      { /* Add left eye */ }
+      <View style = {{...eyeStyle(translatedLeftEyePosition)}} />
+      { /* Add right eye */ }
+      <View style = {{...eyeStyle(translatedRightEyePosition)}} />
+    </View>
   );
 };
 
